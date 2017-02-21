490922a0ac7cf7730ff6c7444dcf835be922713a
Alby <alby@bleary-id.co.uk>
2017-02-21 20:29:13 +0000

    Make multi-line comments single-line
    'Cause that's how I likes it.

--- sketch.ino
+++ sketch.ino
@@ -9,24 +9,22 @@ void setup()
 
 void loop()
 {
-  if (toggle == false)   // When you power up your arduino, boolean "toggle" is set to false, so the following section of code will                          //commence.  The "ELSE" is ignored for now.
+  if (toggle == false)   // When you power up your arduino, boolean "toggle" is set to false, so the following section of code will commence. The "ELSE" is ignored for now.
     { 
       digitalWrite(LED,LOW);        // Turn the LED off
       analogValue = analogRead(0);  // Take an analog reading from A0 line.  No sounds should be offer a returned value of "0"
-      if (analogValue > 8)          // If higher than 8 is returned, then change state of boolean "toggle" to true, display analogValue to                                     // serial monitor and wait 200ms.
-      // The higher the value in the above "if" statement, the less sensitive your clapper will be.  Use the serial monitor to calibrate to       // your wanted sensitivity =D
+      if (analogValue > 8)          // If higher than 8 is returned, then change state of boolean "toggle" to true, display analogValue to serial monitor and wait 200ms. The higher the value in the above "if" statement, the less sensitive your clapper will be.  Use the serial monitor to calibrate to your wanted sensitivity =D
       {
         toggle = true;
         Serial.println(analogValue);
         delay(200);
       }
     }
-    else                            // If boolean "toggle" has beenchanged to true, then the above section of code will be ignored, and the                                     // code below will commence.
+    else                            // If boolean "toggle" has beenchanged to true, then the above section of code will be ignored, and the code below will commence.
     {
       digitalWrite(LED,HIGH);       // Turn the LED on
       analogValue = analogRead(0);  // Take an analog reading from A0 line.  No sounds should be offer a returned value of "0"
-      if (analogValue > 8)          // If higher than 8 is returned, then change state of boolean "toggle" back to false, print analogValue                                     // to serial monitor and wait 200ms.
-      // The higher the value in the above "if" statement, the less sensitive your clapper will be.  Use the serial monitor to calibrate to       // your wanted sensitivity =D
+      if (analogValue > 8)          // If higher than 8 is returned, then change state of boolean "toggle" back to false, print analogValue to serial monitor and wait 200ms. The higher the value in the above "if" statement, the less sensitive your clapper will be.  Use the serial monitor to calibrate to your wanted sensitivity =D
       {
         toggle = false;
         Serial.println(analogValue);
